240c7c860ec3b8f8bae986a0b9f173d9522d7ed3
X7123M3-256 <X123M3-256@users.noreply.github.com>
2015-09-12 21:25:13 +0100

    Removed reversing support routines

--- main.c
+++ main.c
@@ -3,48 +3,11 @@
 #include <gtk/gtk.h>
 #include "mainwindow.h"
 
-
-#include "dat.h"
-#include <string.h>
-//These methods are used to dump various bits of information for reversing purposes
-
-
-
-void dump_unknown_data(object_t* object)
-{
-
-printf("%s\n",string_table_get_string_by_language(object->string_tables[STRING_TABLE_NAME],LANGUAGE_ENGLISH_UK));
-
-int i=0;
-    for(i=0;i<4;i++)
-    {
-    car_t* car=object->ride_header->cars+i;
-    printf("Car %d: %d %d %d %d %d\n",i,car->unknown[0],car->unknown[1],car->unknown[2],car->unknown[3],car->unknown[3]);
-    }
-
-}
-
-#define SRC_DIR "/home/edward/OpenRCT2/RCT2 Reversing/Object DATs/"
-void iterate_files(void (subroutine)(object_t* file))
-{
-DIR           *dir;
-struct dirent *entity;
-dir=opendir(SRC_DIR);
-    while ((entity = readdir(dir)) != NULL)
-    {
-    char path[256]=SRC_DIR;
-    strcat(path,entity->d_name);
-    object_t* object=object_load_dat(path);
-        if(object)
-        {
-        subroutine(object);
-        object_free(object);
-        }
-    }
-closedir(dir);
-}
-
-
+//TODO: Make models appear in menu as soon as they're added
+//TODO: Fix crashing when reexporting
+//TODO: Make labels use actual units
+//TODO: Make animation preview update immediately instead of waiting for slider
+//TODO: Color selection window
 
 
 int main(int argc,char**argv)
