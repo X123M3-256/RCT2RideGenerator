2d7be72e92333eaed8e4012fd83507d06fe2604e
spacek531 <spacek531@comcast.net>
2017-10-10 17:51:11 -0700

    i am bad at punctuation

--- src/animation.c
+++ src/animation.c
@@ -491,7 +491,7 @@ int animation_object_is_descendant_of_rider(animation_object_t* object)
             if (cur_object->model->is_rider) {
                 return 1;
             }
-        while (cur_object != object && cur_object->parent != NULL);
+        } while (cur_object != object && cur_object->parent != NULL);
     }
     return 0;
 }
