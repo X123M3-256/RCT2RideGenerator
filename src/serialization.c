33b061292f41a3306774936afd99ff8cc2def1b9
Michał Janiszewski <janisozaur@gmail.com>
2017-10-16 17:38:52 +0200

    Remove custom strdup

--- src/serialization.c
+++ src/serialization.c
@@ -3,13 +3,6 @@
 #include <assert.h>
 #include <string.h>
 
-char* strdup(const char* str)
-{
-    char* new_str = malloc(strlen(str) + 1);
-    strcpy(new_str, str);
-    return new_str;
-}
-
 static json_t* matrix_serialize(Matrix matrix)
 {
     int i, j;
